95c9a0b2c3bace0d8da0e8eb07c3ca7b0b70405f
Sudongk <aa88sd@naver.com>
2023-09-18 10:37:56 +0900

    ProductRepositoryTest 다양한 정렬 findProductsByCategoryAndOrderByCondition test

--- src/test/java/com/example/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/product/repository/ProductRepositoryTest.java
@@ -39,4 +39,79 @@ class ProductRepositoryTest extends RepositoryTest {
         assertThat(products.stream().allMatch(p -> p.getBrand() != null)).isTrue();
     }
 
+    @Test
+    void findProductsByCategoryAndOrderByCondition_orderBy_wishCount_desc() {
+        // given
+        QueryParameter queryParameter = QueryParameter.of(
+                "",
+                "11000000",
+                "POPULAR",
+                "0",
+                "20"
+        );
+
+        // when
+        Page<Product> products = productRepository.findProductsByCategoryAndOrderByCondition(queryParameter.getCategory().getId(), queryParameter.getPageRequest());
+
+        // then
+        assertThat(products).isNotNull();
+
+        // orderBy가 "REVIEW"인지 확인
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("wishCount")).getProperty()).isEqualTo("wishCount");
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("wishCount")).getDirection()).isEqualTo(Sort.Direction.DESC);
+
+        // fetch join을 사용하여 연관 관계 엔티티에 대한 추가 쿼리가 발생하지 않는지 확인
+        assertThat(products.stream().allMatch(p -> p.getBrand() != null)).isTrue();
+    }
+
+    @Test
+    void findProductsByCategoryAndOrderByCondition_orderBy_reviewAvg_desc() {
+        // given
+        QueryParameter queryParameter = QueryParameter.of(
+                "",
+                "11000000",
+                "REVIEW_AVG",
+                "0",
+                "20"
+        );
+
+        // when
+        Page<Product> products = productRepository.findProductsByCategoryAndOrderByCondition(queryParameter.getCategory().getId(), queryParameter.getPageRequest());
+
+        // then
+        assertThat(products).isNotNull();
+
+        // orderBy가 "REVIEW"인지 확인
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("reviewAvg")).getProperty()).isEqualTo("reviewAvg");
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("reviewAvg")).getDirection()).isEqualTo(Sort.Direction.DESC);
+
+        // fetch join을 사용하여 연관 관계 엔티티에 대한 추가 쿼리가 발생하지 않는지 확인
+        assertThat(products.stream().allMatch(p -> p.getBrand() != null)).isTrue();
+    }
+
+    @Test
+    void findProductsByCategoryAndOrderByCondition_orderBy_createdAt_desc() {
+        // given
+        QueryParameter queryParameter = QueryParameter.of(
+                "",
+                "11000000",
+                "RECENT",
+                "0",
+                "20"
+        );
+
+        // when
+        Page<Product> products = productRepository.findProductsByCategoryAndOrderByCondition(queryParameter.getCategory().getId(), queryParameter.getPageRequest());
+
+        // then
+        assertThat(products).isNotNull();
+
+        // orderBy가 "REVIEW"인지 확인
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("createdAt")).getProperty()).isEqualTo("createdAt");
+        assertThat(Objects.requireNonNull(products.getSort().getOrderFor("createdAt")).getDirection()).isEqualTo(Sort.Direction.DESC);
+
+        // fetch join을 사용하여 연관 관계 엔티티에 대한 추가 쿼리가 발생하지 않는지 확인
+        assertThat(products.stream().allMatch(p -> p.getBrand() != null)).isTrue();
+    }
+
 }
