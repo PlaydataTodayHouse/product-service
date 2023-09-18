314a4c681825647d0bd6b5003732c84d05cb3987
Sudongk <aa88sd@naver.com>
2023-09-18 09:12:28 +0900

    WebConfig 추가

--- src/main/java/com/example/product/config/WebConfig.java
+++ src/main/java/com/example/product/config/WebConfig.java
@@ -1,4 +1,32 @@
 package com.example.product.config;
 
-public class WebConfig {
+import com.example.product.domain.entity.dto.request.query.QueryParamsArgResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final QueryParamsArgResolver queryParamsArgResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(queryParamsArgResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
 }
